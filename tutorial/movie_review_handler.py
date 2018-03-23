28ef15bc7b234720feaa0e031e16cf4ff6a2e62a
刘宏亮 <liuhongliang04@meituan.com>
2018-03-23 20:32:18 +0800

    初步菜单

--- tutorial/movie_review_handler.py
+++ tutorial/movie_review_handler.py
@@ -1,4 +1,5 @@
 # _*_ coding:utf-8 _*_
+import json
 import logging
 
 import sys
@@ -19,12 +20,66 @@ class MovieReview():
         pass
 
     def main(self):
-        global search_text
-        search_text = raw_input('请输入要搜索的关键词：')
-        # # 爬取结果
         process = CrawlerProcess(get_project_settings())
-        process.crawl(SearchSpider,search_text)
-        process.start()  # the script will block here until the crawling is finished
+        search_text = self.search_film_by_keyword(process)
+        # Reading data back
+        search_result = self.load_result()
+        film_name = search_result['film_name']
+        film_url = search_result['film_url']
+        previous_page = search_result['previous_page']
+        next_page = search_result['next_page']
+        self.show_search_result(film_name, film_url)
+        print 'p:上一页 n:下一页 q：退出'
+        select_key = raw_input("请选择要分析的电影：")
+        while select_key != 'q':
+            if select_key == 'n':
+                self.crawl_data(process, search_text,next_page)
+                search_result = self.load_result()
+                film_name = search_result['film_name']
+                film_url = search_result['film_url']
+                previous_page = search_result['previous_page']
+                next_page = search_result['next_page']
+                self.show_search_result(film_name, film_url)
+            if select_key == 'p':
+                self.crawl_data(process,search_text,previous_page)
+                search_result = self.load_result()
+                film_name = search_result['film_name']
+                film_url = search_result['film_url']
+                previous_page = search_result['previous_page']
+                next_page = search_result['next_page']
+                self.show_search_result(film_name, film_url)
+            if select_key.isalpha():
+                print self.parse_subject_id(film_url,int(select_key))
+
+
+    def show_search_result(self, film_name, film_url):
+        try:
+            sequence = 1;
+            for i in xrange(len(film_url)):
+                if "subject" in film_url[i]:
+                    subject_id = self.parse_subject_id(film_url, i)
+                    print "%d) name=%s subject_id=%s" % (sequence, film_name[i], subject_id)
+                    sequence += 1
+        except Exception as e:
+            print e
+
+    def parse_subject_id(self, film_url, i):
+        return film_url[i].split('subject')[1].replace('/', '')
+
+    def load_result(self):
+        with open('film_search_result.json', 'r') as f:
+            search_result = json.load(f)
+        return search_result
+
+    def search_film_by_keyword(self, process):
+        search_text = raw_input('请输入要搜索的关键词：')
+        self.crawl_data(process, search_text)
+        return search_text
+
+    def crawl_data(self, process, search_text, suffix=''):
+        # 爬取结果
+        process.crawl(SearchSpider, search_text, suffix)
+        process.start(stop_after_crawl=False)  # the script will block here until the crawling is finished
 
 
 if __name__ == '__main__':
--- tutorial/spiders/search_spider.py
+++ tutorial/spiders/search_spider.py
@@ -11,12 +11,13 @@ class SearchSpider(scrapy.Spider):
     name = "douban_search"
     allowed_domains = ["douban.com"]
 
-    def __init__(self, search_text):
+    def __init__(self, search_text,suffix):
         self.search_text = search_text
+        self.suffix=suffix
 
     def start_requests(self):
         urls = [
-            ur'https://movie.douban.com/subject_search?search_text=' + self.search_text + ur'&cat=1002',
+            ur'https://movie.douban.com/subject_search?search_text=' + self.search_text + ur'&cat=1002'+self.suffix,
         ]
         user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0'
         headers = {'User-Agent': user_agent}
@@ -33,9 +34,8 @@ class SearchSpider(scrapy.Spider):
             item_loader.add_xpath('film_name', '//div[@class="title"]/a/text()')
             item_loader.add_xpath('film_url', '//div[@class="title"]/a/@href')
             item = item_loader.load_item()
-            item['previous_page'] = response.urljoin(
-                response.xpath('//a[@class="prev activate"]/@href').extract_first())
-            item['next_page'] = response.urljoin(response.xpath('//a[@class="next"]/@href').extract_first())
+            item['previous_page'] = response.xpath('//a[@class="prev activate"]/@href').extract_first()
+            item['next_page'] = response.xpath('//a[@class="next"]/@href').extract_first()
         except Exception as e:
             print e
         yield item
